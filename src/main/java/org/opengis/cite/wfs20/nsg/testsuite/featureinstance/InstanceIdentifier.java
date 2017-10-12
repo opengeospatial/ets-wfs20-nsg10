f3029395b64e0775fc7e3cddc953d51e2621852e
Lyn Elisa Goltz <goltz@lat-lon.de>
2017-10-12 09:36:18 +0200

    #4906 - implementation of requirement 9

---
+++ src/main/java/org/opengis/cite/wfs20/nsg/testsuite/featureinstance/InstanceIdentifier.java
@@ -0,0 +1,47 @@
+package org.opengis.cite.wfs20.nsg.testsuite.featureinstance;
+
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.opengis.cite.iso19142.BaseFixture;
+import org.opengis.cite.iso19142.SuiteAttribute;
+import org.opengis.cite.iso19142.util.DataSampler;
+import org.testng.ISuite;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.opengis.cite.wfs20.nsg.utils.ValidationUtils.assertUuidOrGuideId;
+
+/**
+ * Contains test for the feature instance identifier.
+ *
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class InstanceIdentifier extends BaseFixture {
+
+    private DataSampler dataSampler;
+
+    /**
+     * Obtains a DataSampler object from the test run context (the value of the {@link SuiteAttribute#SAMPLER
+     * SuiteAttribute.SAMPLER attribute}).
+     * 
+     * @param testContext
+     *            The test run context.
+     */
+    @BeforeClass(alwaysRun = true)
+    public void initTransactionFixture( ITestContext testContext ) {
+        ISuite suite = testContext.getSuite();
+        this.dataSampler = (DataSampler) suite.getAttribute( SuiteAttribute.SAMPLER.getName() );
+    }
+
+    @Test(description = "See NSG WFS 2.0 Profile: Requirement 9", dataProvider = "feature-types")
+    public void featureInstanceIdentifier( QName featureType ) {
+        Set<String> ids = this.dataSampler.selectRandomFeatureIdentifiers( featureType, 10 );
+        for ( String id : ids ) {
+            assertUuidOrGuideId( id );
+        }
+    }
+
+}
---
+++ src/main/java/org/opengis/cite/wfs20/nsg/utils/ValidationUtils.java
@@ -0,0 +1,33 @@
+package org.opengis.cite.wfs20.nsg.utils;
+
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="mailto:goltz@lat-lon.de">Lyn Goltz </a>
+ */
+public class ValidationUtils {
+
+    private static final String UUID_PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$";
+
+    // guide://{prefix}/{suffix}
+    private static final String GUIDE_ID_PATTERN = "^guide://[0-9a-z]*/[0-9a-z]*";
+
+    /**
+     * Validates if the passed id is not null and a valid UUID or GUIDE ID identifier. The format of the GUIDE id is as
+     * follows: guide://{prefix}/{suffix}
+     * 
+     * @param id
+     *            the id to validate
+     */
+    public static void assertUuidOrGuideId( String id ) {
+        assertNotNull( id, "Identifier must not be null" );
+
+        boolean idUuid = Pattern.matches( UUID_PATTERN, id );
+        boolean isGuideId = Pattern.matches( GUIDE_ID_PATTERN, id );
+        assertTrue( idUuid || isGuideId, "Identifier must be a valid UUID or GUIDE ID" );
+    }
+
+}
